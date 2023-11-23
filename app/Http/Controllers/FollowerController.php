e731b16ab2adce5cb64f2712cef0944cadd1d24a
Azza-arch <haziqsyed00@gmail.com>
2023-11-23 14:34:29 +0800

    arrange & comment

--- app/Http/Controllers/FollowerController.php
+++ app/Http/Controllers/FollowerController.php
@@ -13,54 +13,34 @@ class FollowerController extends Controller
         return view("news.profile");
     }
 
+    //folloe user function
     public function storefollow(Request $request)
     {
+        //defining
         $follow = new Follows;
+        //check for authentication of the user
         $user = Auth::user();
         $follow->user_id = $user->id;
         $follow->following_id = $request->id;
+        //follow a user
         $follow->save();
         return back()
             ->with('success', 'You are now following this user');
         //
     }
 
-    /**
-     * Display the specified resource.
-     */
-    // public function shows()
-    // {
-    //     $user = Auth::user();
-    //     $followers = $user->followers;
-    //     return view('news.profile',compact("followers"));
-    // }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
+    //unfollow user
     public function destroy($id)
     {
+        //check for authentication of the user
         if (Auth::check()) {
+            //unfollow
             $unfollow = Follows::where([['user_id', auth()->user()->id], ['following_id', $id]])
                 ->delete();
+            //if unfollow then it goes back
             if ($unfollow) {
-                return back()->with('success','You have unfollow this person');
+                return back()->with('success', 'You have unfollow this person');
+                //if not
             } else {
                 return redirect('/profile/' . auth()->user()->username);
             };
--- app/Http/Controllers/NewsController.php
+++ app/Http/Controllers/NewsController.php
@@ -9,51 +9,78 @@ use Illuminate\Support\Facades\Auth;
 
 class NewsController extends Controller
 {
+    // Display the news index page
     public function index()
     {
         return view("news.index");
     }
-    //
+
+    // Store a new news article
     public function storenews(Request $request): RedirectResponse
     {
+        // Validate the incoming request data
         $request->validate([
-            'title' =>'required',
+            'title' => 'required',
             'description' => 'required|max:255',
         ]);
 
+        // Create a new News instance
         $news = new News;
-        $news->id = $request->id;
+        
+        // Assign values to News attributes from the request
         $news->title = $request->title;
         $news->description = $request->description;
         $news->image = $request->image;
+        
+        // Assign the authenticated user's id to the news article
         $news->user_id = auth()->user()->id;
+
+        // Save the news article to the database
         $news->save();
-        return redirect("news")->with('success','You have post the news');
+
+        // Redirect with a success message
+        return redirect("news")->with('success', 'You have posted the news');
     }
 
+    // Delete a news article
     public function deletenews($id)
     {
+        // Check if the user is authenticated
         if (Auth::check()) {
+            // Find the news article by its id
             $news = News::findOrFail($id);
-            $news->comments()->delete(); // Delete associated comments
+            
+            // Delete associated comments
+            $news->comments()->delete();
+            
+            // Delete the news article
             $news->delete();
-            return back()->with('success','You have delete the news');
+
+            // Redirect back with a success message
+            return back()->with('success', 'You have deleted the news');
         }
     }
 
+    // Show news articles associated with the authenticated user
     public function showmy()
     {
         // Get the authenticated user
         $user = Auth::user();
+        
         // Retrieve news articles associated with the authenticated user
         $news = $user->news;
+        
+        // Return the profile view with the associated news articles
         return view("news.profile", compact("news"));
     }
 
+    // Show all news articles
     public function showNews()
     {
-
+        // Retrieve all news articles in descending order by creation date
         $news = News::orderBy('created_at', 'desc')->get();
-        return view("news\index", compact("news"));
+        
+        // Return the news index view with the retrieved news articles
+        return view("news.index", compact("news"));
     }
 }
